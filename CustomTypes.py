1a73d4673b9e0d863d9cf11b679ddce4d9433316
ellie :3 <ellsylv@proton.me>
2025-12-09 21:30:05 -0500

    idk explicit 128-bit unsigned ints seems interesting

--- CustomTypes.py
+++ CustomTypes.py
@@ -7,6 +7,7 @@ class char:
     def __repr__(self) -> str:
         return self.value
 
+
 class String:
     def __init__(self, size: int, value: str = ""):
         self.size = size
@@ -150,6 +151,7 @@ class uint8:
 
         return other.__int__() ** self.value
 
+
 class uint16:
     def __init__(self, value: int) -> None:
         if value > 2 ** 16 - 1 or value < 0:
@@ -272,6 +274,7 @@ class uint16:
             raise Exception("ERROR: Overflow detected")
 
         return other.__int__() ** self.value
+
 
 class uint32:
     def __init__(self, value: int) -> None:
@@ -397,6 +400,7 @@ class uint32:
 
         return other.__int__() ** self.value
 
+
 class uint64:
     def __init__(self, value: int) -> None:
         self.max_value = 2**64 - 1
@@ -520,6 +524,131 @@ class uint64:
             raise Exception("ERROR: Overflow detected")
 
         return other.__int__() ** self.value
+
+class uint128:
+    def __init__(self, value: int) -> None:
+        self.max_value = 2**128 - 1
+        if value > 2 ** 64 - 1 or value < 0:
+            del self
+            Exception(f"ERROR: Cannot assign value {value} to unsigned 128-bit integer.")
+        else:
+            self.value = value
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
+    def __add__(self, other) -> int:
+        if self.value + other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value + other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value + other.__int__()
+
+    def __radd__(self, other) -> int:
+        if self.value + other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value + other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value + other.__int__()
+
+    def __sub__(self, other) -> int:
+        if self.value - other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value - other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value - other.__int__()
+
+    def __rsub__(self, other) -> int:
+        if other.__int__() - self.value > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif other.__int__() - self.value < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return other.__int__() - self.value
+
+    def __mul__(self, other) -> int:
+        if self.value * other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value * other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value * other.__int__()
+
+    def __rmul__(self, other) -> int:
+        if self.value * other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value * other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value * other.__int__()
+
+    def __truediv__(self, other) -> int:
+        if self.value / other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value / other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value / other.__int__()
+
+    def __rtruediv__(self, other) -> int:
+        if other.__int__() / self.value > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif other.__int__() / self.value < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return other.__int__() / self.value
+
+    def __mod__(self, other) -> int:
+        if self.value % other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value % other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value % other.__int__()
+
+    def __rmod__(self, other) -> int:
+        if other.__int__() % self.value > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif other.__int__() % self.value < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return other.__int__() % self.value
+
+    def __floordiv__(self, other) -> int:
+        if self.value // other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value // other.__int__() < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return self.value // other.__int__()
+
+    def __rfloordiv__(self, other) -> int:
+        if other.__int__() // self.value > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif other.__int__() // self.value < 0:
+            raise Exception("ERROR: Underflow detected")
+
+        return other.__int__() // self.value
+
+    def __pow__(self, other) -> int:
+        if self.value ** other.__int__() > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif self.value ** other.__int__() < 0:
+            raise Exception("ERROR: Overflow detected")
+
+        return self.value ** other.__int__()
+
+    def __rpow__(self, other) -> int:
+        if other.__int__() ** self.value > self.max_value:
+            raise Exception("ERROR: Overflow detected")
+        elif other.__int__() ** self.value < 0:
+            raise Exception("ERROR: Overflow detected")
+
+        return other.__int__() ** self.value
+
 
 class uintarb:
     def __init__(self, value: int, bits: int) -> None:
